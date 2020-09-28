06571035858aba501306d22bfd31944c038b0fd5
SweetSheldon <48868754+SweetSheldon@users.noreply.github.com>
2020-09-29 00:30:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array){
+  if(array===undefined||array[0]===undefined)return 0;
+  var minNum = array[0];
+  for(i =1;i<array.length;i++){
+   if(minNum>array[i]){minNum=array[i];}
+  }
+  return minNum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array===undefined||array[0]===undefined)return 0;
+  var maxNum = array[0];
+  for(i =1;i<array.length;i++){
+   if(maxNum<array[i]){maxNum=array[i];}
+  }
+  return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array===undefined||array[0]===undefined)return 0;
+  var sum=0;
+  for(i =0;i<array.length;i++){
+   sum+=array[i];
+   }
+   return sum/array.length;
 }
